837e962bc4350ea64cf3c9fbfb4ac2129a921350
Adam Greloch <zplhatesbananas@gmail.com>
2025-01-11 21:39:52 +0100

    add two screens

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -19,19 +19,48 @@ export default function Home() {
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        {/* not intended to be left this way, just collecting these phone layouts in one place */}
         <SmartPhone>
-          <div className="flex flex-col items-center gap-4">
-            <Image
-              className="mt-10 self-center"
-              src="/next.svg"
-              alt="Next.js logo"
-              width={180}
-              height={38}
-              priority
-            />
-            <h1 className="text-3xl font-bold text-center mt-4">
-              Wnucheck
-            </h1>
+          <div className="flex p-4 flex-col items-center gap-10">
+            <div className="items-center gap-10">
+              <p className="text-5xl font-bold text-center mt-4">
+                213 721 372
+              </p>
+              <p className="text-3xl text-center mt-4">
+                Niezaufany numer
+              </p>
+            </div>
+            <div className="p-6 max-w-sm my-3 mx-auto bg-red-500 rounded-xl shadow-lg flex items-center gap-x-4">
+              <div>
+                <div className="text-3xl font-medium text-black">Uwaga!</div>
+                <p className="text-xl text-slate-800 mt-1">Tego numeru nie ma w twojej skrzynce telefonicznej. To może być oszust</p>
+              </div>
+            </div>
+            <div className="flex-auto flex space-x-4">
+              <button className="h-40 px-10 font-semibold rounded-md bg-slate-600 text-white hover:bg-slate-900" type="submit">
+                Odbierz
+              </button>
+              <button className="h-40 px-10 font-semibold rounded-md border border-red-700 text-red-700 hover:bg-red-900 hover:text-white" type="button">
+                Odrzuć
+              </button>
+            </div>
+          </div>
+        </SmartPhone>
+        <SmartPhone>
+          <div className="flex p-4 flex-col items-center gap-10">
+            <div className="m-2 max-w-sm mx-auto rounded-xl shadow-lg flex items-center grid divide-y border border-red-700">
+              <div className="p-4 font-medium text-black text-xl"> Nowa wiadomość</div>
+              <div className="p-4 bg-red-500 flex items-center gap-x-4">
+                <div>
+                  <div className="text-2xl font-medium text-black">Uwaga!</div>
+                  <p className="text-slate-800">Numer nadawcy jest niezaufany. To może być próba oszustwa</p>
+                </div>
+              </div>
+              <div className="p-4">
+                <div className="text-xl font-medium text-black">213 721 372</div>
+                <p className="text-slate-500">Hej dziadku, tu wnuczek pilnie potrzebuje pieniedzy przekazesz mi 2000zl prosze????!!</p>
+              </div>
+            </div>
           </div>
         </SmartPhone>
       </main>
